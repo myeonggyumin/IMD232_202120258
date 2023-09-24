b502ab66b47e1ab32be7214d0b65599f78b88f98
myeonggyumin <143991225+myeonggyumin@users.noreply.github.com>
2023-09-24 22:07:39 +0900

    4주차 스케치

---
+++ src/w4/sketches/sketch1/script/sketch.js
@@ -0,0 +1,23 @@
+let mover;
+let wind;
+let gravity;
+
+function setup() {
+  setCanvasContainer('mySketchGoesHere', 3, 2, true);
+  background(255);
+  mover = new MoverNoMass(width / 2, height / 2, 50);
+  wind = createVector(0.2, 0);
+  gravity = createVector(0, 0.1);
+}
+
+function draw() {
+  background(255);
+  mover.addAcc(gravity);
+  if (mouseIsPressed && isMouseInsideCanvas()) {
+    mover.addAcc(wind);
+  }
+  mover.update();
+  mover.checkEdges();
+  mover.display();
+  mover.displayVectors();
+}
