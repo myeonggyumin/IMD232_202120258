1a14c17c7c05a1386343068d564f9ac46e1a9872
myeonggyumin <143991225+myeonggyumin@users.noreply.github.com>
2023-10-15 16:37:11 +0900

    5주차과제

---
+++ src/w5/assignment/step1/script/mover.js
@@ -0,0 +1,63 @@
+const cNum = 8;
+const rNum = 8;
+const gridSize = 60;
+let angleIncrement = 15;
+
+let angle;
+let angleVel;
+
+function periodToAngleVel(periodAsFrame) {
+  return TAU / periodAsFrame;
+}
+
+function setup() {
+  setCanvasContainer('canvas', 1, 1, true);
+  colorMode(HSL, 360, 100, 100, 100);
+  background(360, 0, 100);
+
+  angle = createVector(0, TAU / 4);
+  angleVel = createVector(periodToAngleVel(360), periodToAngleVel(360));
+}
+
+function drawRotatingGraphic(x, y, initialAngle) {
+  let amplitude = [gridSize / 2 - 10, gridSize / 2 - 10];
+
+  stroke(0);
+  strokeWeight(1);
+  fill(255);
+  ellipse(x, y, gridSize - 15);
+
+  stroke(0);
+  strokeWeight(1);
+  fill(255);
+  line(
+    x,
+    y,
+    x + sin(angle.x + radians(initialAngle)) * amplitude[0],
+    y + sin(angle.y + radians(initialAngle)) * amplitude[1]
+  );
+
+  stroke(0);
+  strokeWeight(1);
+  fill(0);
+  ellipse(
+    x + sin(angle.x + radians(initialAngle)) * amplitude[0],
+    y + sin(angle.y + radians(initialAngle)) * amplitude[1],
+    10
+  );
+}
+
+function draw() {
+  background(360, 0, 100);
+  let prevRowLastAngle = 0;
+
+  for (let r = 0; r < rNum; r++) {
+    for (let c = 0; c < cNum; c++) {
+      const x = c * (gridSize + 20) + gridSize;
+      const y = r * (gridSize + 20) + gridSize;
+      drawRotatingGraphic(x, y, angleIncrement * (c + r) + prevRowLastAngle);
+    }
+    prevRowLastAngle += 120;
+  }
+  angle.add(angleVel);
+}
