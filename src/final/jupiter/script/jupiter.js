4ce55ff2dbeff37e1f5289d479bdd05d49150780
myeonggyumin <143991225+myeonggyumin@users.noreply.github.com>
2023-12-14 22:52:08 +0900

    ㅁㅇㄹ

--- src/final/jupiter/script/jupiter.js
+++ src/final/jupiter/script/jupiter.js
@@ -63,12 +63,31 @@
 let debug = true;
 let flowfield;
 let vehicles = [];
+let vehicles2 = [];
 let lastChangeTime = 0;
 let app;
+let img;
+
+function preload() {
+  img = loadImage('1.png');
+}
 
 function setup() {
   setCanvasContainer('canvas', 1, 1, true);
   flowfield = new FlowField(20, 0.1);
+  for (let i = 0; i < 60; i++) {
+    vehicles2.push(
+      new Vehicle2(
+        random(width),
+        random(height),
+        1,
+        4,
+        random(2, 5),
+        random(0.1, 0.5)
+      )
+    );
+  }
+
   for (let i = 0; i < 7000; i++) {
     vehicles.push(
       new Vehicle(
@@ -85,6 +104,13 @@ function setup() {
 
 function draw() {
   background('#E3BA94');
+
+  vehicles2.forEach((each) => {
+    each.follow(flowfield);
+    each.update();
+    each.borderInfinite();
+    each.display();
+  });
 
   vehicles.forEach((each) => {
     each.follow(flowfield);
